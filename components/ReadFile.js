3450e9711009d3bff669ea1d0062b0842bda3ef5
coderider <code.rider@hotmail.com>
2014-12-22 17:55:00 +0500

    reader js
    reader js

---
+++ components/ReadFile.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+var noflo = require('noflo');
+
+exports.getComponent = function () {
+  var c = new noflo.Component();
+
+  c.inPorts.add('in', function (event, payload) {
+    if (event !== 'data') {
+      return;
+    }
+	var alltext = function readTextFile(file){	  
+      var rawFile = new XMLHttpRequest();
+      rawFile.open("GET", file, false);
+      rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                return rawFile.responseText;
+                //alert(allText);
+            }
+        }
+      }
+      rawFile.send(null);
+    }
+    // Do something with the packet, then
+    c.outPorts.out.send(alltext("text.txt"));
+  });
+  c.outPorts.add('out');
+  return c;
+};
